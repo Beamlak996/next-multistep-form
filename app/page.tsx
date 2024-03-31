f13e3bf80c773275dc31fc14bd022a84a1fc1f0f
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-03-31 05:22:07 -0900

    form added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { MultistepForm } from "@/components/multistep-form";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <section className="py-24">
-      <div className="container">Hello there</div>
+      <div className="container">
+        <MultistepForm />
+      </div>
     </section>
   );
 }
